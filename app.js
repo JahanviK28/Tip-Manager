64f50e2d0fb633bf94a9497c715e0974c296ab29
jahanvi <janvik2809@gmail.com>
2024-05-26 13:51:39 +0530

    firts commit

--- app.js
+++ app.js
@@ -14,4 +14,4 @@ app.use('/api/tip', tipRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
