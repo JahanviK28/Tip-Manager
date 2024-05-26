5b20d521a1fd8d658eb209f4eb754d0888612a3f
kevit-jahanvi-khakhar <jahanvi.khakhar@kevit.io>
2024-05-26 13:42:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDB = require('./config/db.connection');
+const userRoutes = require('./routes/user.route');
+const tipRoutes = require('./routes/tip.route')
+require('dotenv').config();
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+app.use('/api/user', userRoutes);
+app.use('/api/tip', tipRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
